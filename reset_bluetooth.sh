6dc7ec889d841b5018870c8f5334f5b0a93e0fb3
Vitor Nunes <vitornovictor@gmail.com>
2024-06-04 16:13:01 +0100

    Simplify the script

--- reset_bluetooth.sh
+++ reset_bluetooth.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
 # Restart Bluetooth
+echo "Restarting bluetooth"
 sudo service bluetooth restart
 
 # Reload modules 
+echo "Reloading modules"
 sudo rmmod btusb
 sudo modprobe btusb
 
 # Unblock if needed
+echo "Unblocking connections"
 sudo rfkill unblock bluetooth
-
-# Start the bluetoothctl interactive command
-# bluetoothctl << EOF
-# # List all paired devices to get their MAC addresses
-# devices
-
-# # Now loop through each device and remove it
-# for device in $(bluetoothctl devices | cut -f 2 -d ' ')
-# do
-#     echo "Removing device: $device"
-#     # Remove the paired device
-#     remove $device
-# done
-# EOF
-
-# echo "All paired Bluetooth devices have been removed."
